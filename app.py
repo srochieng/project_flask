98730b1be60995c0bfc78c7e5f84fbc5f2ffc9dc
srochieng <srnyambok@students.uonbi.ac.ke>
2021-05-18 21:17:35 +0300

    modified to use basic Jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='Stem Clothing Kenya')
 
 @app.route('/Stem')
 def Stem():
-    return "hello welcome to Stem.clothing!"
+    return render_template('Stem.html', pageTitle='What We Sell')
 
 if __name__ == '__main__':
     app.run(debug=True)
